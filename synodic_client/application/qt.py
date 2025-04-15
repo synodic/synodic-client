ab9f29c60fedd31355d0ccb83b505b52130b908c
Asher Norland <asher.norland@gmail.com>
2025-04-15 01:38:00 -0400

    Add Porringer Calls

--- synodic_client/application/qt.py
+++ synodic_client/application/qt.py
@@ -1,7 +1,10 @@
 """gui"""
 
+import logging
 from typing import LiteralString
 
+from porringer.api import API, APIParameters
+from porringer.schema import ListPluginsParameters, LocalConfiguration
 from PySide6.QtWidgets import QApplication
 
 from synodic_client.application.screen.screen import Screen
@@ -14,6 +17,15 @@ icon: LiteralString = 'icon.png'
 def application() -> None:
     """Entrypoint"""
     client = Client()
+
+    logger = logging.getLogger('synodic_client')
+
+    local_config = LocalConfiguration()
+    api_params = APIParameters(logger)
+    porringer = API(local_config, api_params)
+
+    list_params = ListPluginsParameters()
+    list_results = porringer.plugin.list(list_params)
 
     app = QApplication([])
     app.setQuitOnLastWindowClosed(False)
